6e8709d7285696c6585e29d5a1a289f0c08ef715
vayers <violetlastname@gmail.com>
2024-02-12 11:05:28 -0500

    implement loop validation

--- brainfuck.c
+++ brainfuck.c
@@ -13,25 +13,39 @@ typedef struct {
     int pointer;
 } Loops;
 
-void load_program(FILE* program_file, char* program) {
+int load_program(FILE* program_file, char* program) {
     char* token;
     int i = 0;
 
-    Loops loop_list;
-
-    loop_list.list = malloc(LOOP_LIST_LENGTH * sizeof(char));
-    loop_list.pointer = 0;
+    int loops = 0;
 
     while (fscanf(program_file, "%c", token) != EOF) {
 
         if (strpbrk(BF_TOKENS, token) != 0) {
-            printf("%d\n", i);
             *(program + i) = *token;
             i++;
+
+            if (*token == '[') {
+                loops++;
+            }
+            else if (*token == ']') {
+                if (loops <= 0) {
+                    return 1;
+                }
+                loops--;
+            }
         }
     }
 
-    free(loop_list.list);
+    if (loops > 0) {
+        return 2;
+    }
+
+    return 0;
+}
+
+void interpret_program (char* program, char* tape) {
+
 }
 
 int main(int argc, char *argv[])
@@ -45,7 +59,16 @@ int main(int argc, char *argv[])
     FILE* program_file = fopen(argv[1], "r");
     char* program = malloc(PROGRAM_LENGTH * sizeof(char));
     
-    load_program(program_file, program);
+    switch (load_program(program_file, program)) {
+        case 1:
+            printf("Loop closed, but not started!\n");
+            return 0;
+        break;
+        case 2:
+            printf("Loop started, but not closed!\n");
+            return 0;
+        break;
+    }
     printf("%s\n", program);
 
     return 0;
